e9bd9917d9318d6eecc18c74554be65d826643a3
Phạm Đình Thanh Bình <thanhbinhtk052@gmail.com>
2024-03-05 17:42:09 +0700

    add first Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountEntity } from './auth/account.entity';
+import { ProfileEntity } from './auth/profile.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'admin',
+      password: '12345678',
+      database: 'tomi_database',
+      entities: [AccountEntity, ProfileEntity],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    // .setTitle('Cats example')
+    // .setDescription('The cats API description')
+    // .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(3000, () => {
+    console.info(`Server is running on port 3000`);
+    console.info(`Docs: http://localhost:3000/docs`);
+  });
 }
 bootstrap();
